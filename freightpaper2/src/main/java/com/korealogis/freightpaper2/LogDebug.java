80797f30581f0b0d3d5ab1b5c862579e5f109f3b
jongseo <zizzyguy@gmail.com>
2018-08-29 10:58:15 +0900

    Create project in Studio. Create LogDebug for easy debugging.

---
+++ freightpaper2/src/main/java/com/korealogis/freightpaper2/LogDebug.java
@@ -0,0 +1,12 @@
+package com.korealogis.freightpaper2;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "zest";
+
+    public static void d(String message){
+        Log.d(TAG, message);
+    }
+}
