2349706d14affb675b04892128738dbdf27d88ab
jongseo <zizzyguy@gmail.com>
2018-09-06 14:36:28 +0900

    0.2.2

--- app/src/main/java/com/korealogis/freightpaper/ActivityMain.java
+++ app/src/main/java/com/korealogis/freightpaper/ActivityMain.java
@@ -1,9 +1,10 @@
 package com.korealogis.freightpaper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class ActivityMain extends AppCompatActivity {
+
+public class ActivityMain extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
